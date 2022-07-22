21fc79b77d52b0d6cb0c5b6285e2653b11617ccd
Dizarimia <lillemor.sunnegard@outlook.com>
2022-07-22 21:14:33 +0000

    Add instructions format

--- run.py
+++ run.py
@@ -20,12 +20,26 @@ def instructions():
     """
     Welcome and instructions
     """
+    print("Would you like instructions to the game? Press Y for Yes and N for No.")
+    instruction = input()
+    while instruction.upper() not in ["Y", "N"]:
+    print("Invalid input, choose between Y & N")
+    instruction = input()
+    if instruction.upper() == "Y":
+        return instructions()
+    print("To play hangwoman, what you need to do is guess the word one letter at a time."
+    "Insert a letter and hit enter. If it is correct the letter gets added to the word."
+    "If your guess is wrong a part of the hangwoman image will be added." 
+    "Keep guessing until you get the whole word or you run out of tries")
 
-print("To play hangwoman, what you need to do is guess the word one letter at a time. Insert a letter and hit enter. If it is correct the letter gets added to the word. If your guess is wrong a part of the hangwoman image will be added. Keep guessing until you get the whole word or you run out of tries")
+    else:
+        return startup()
+
+
 
 
 def startup():
-    print("Welcome! Please choose difficulty: E Easy or H Hard")
+    print("Welcome! Please choose difficulty: E for Easy or H for Hard")
     difficulty = input()
     while difficulty.upper() not in ["H", "E"]:
         print("Invalid input, choose between E & H")
