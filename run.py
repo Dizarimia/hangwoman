297349dd43a2cfa1169a39eb8803df6aabb12b62
Dizarimia <lillemor.sunnegard@outlook.com>
2022-07-22 19:20:18 +0000

    Add instructions

--- run.py
+++ run.py
@@ -16,6 +16,14 @@ def title():
     print("HANGWOMAN")
 
 
+def instructions():
+    """
+    Welcome and instructions
+    """
+
+print("To play hangwoman, what you need to do is guess the word one letter at a time. Insert a letter and hit enter. If it is correct the letter gets added to the word. If your guess is wrong a part of the hangwoman image will be added. Keep guessing until you get the whole word or you run out of tries")
+
+
 def startup():
     print("Welcome! Please choose difficulty: E Easy or H Hard")
     difficulty = input()
