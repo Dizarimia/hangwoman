84685fc1f681605e9c15d7ad7effc604940fdd7c
Dizarimia <lillemor.sunnegard@outlook.com>
2022-07-23 20:27:04 +0000

    Add loop for guesses

--- run.py
+++ run.py
@@ -27,7 +27,7 @@ def instructions():
     instruction = input()
     if instruction.upper() == "Y":
         return instructions()
-    print("To play hangwoman, you need to guess the word one letter at a time."
+     print(f"To play hangwoman, you need to guess the word one letter at a time."
     "Press a letter and hit enter. If it's correct it gets added to the word."
     "If your guess is wrong a part of the hangwoman image will be added."
     "Keep guessing until you get the whole word or you run out of tries")
@@ -47,6 +47,42 @@ def startup():
         return hard_words
     else:
         return easy_words
+    
+guesses = []   
+LetGuessed = "" 
+tries = 6
+while tries > 0:
+    guess = input("Enter a letter: ")
+
+    if guess in word: 
+        print(f"YAY! There is one or more {guess} in the word.")
+    else:
+        tries -= 1
+        print(f"NOPE! No {guess} in the word. {tries} turn(s) left.")
+
+    LetGuessed = LetGuessed + guess
+    wrongGuess = 0
+
+
+
+
+def display_word(word, guesses):
+    display = ""
+    for letter in word:
+        if letter in LetGuessed:
+            print(f"{letter}", end="")
+        else:
+            print(f"_", end="")
+            wrongGuess += 1
+
+    if wrongGuess == 0:
+        print(f"WOHO! The word was: {pick}. You win!")
+        break
+
+        display = display + " " + letter
+    print(display)
+    while tries > 0 and len(words) > 0:
+
 
 
 def replay():
@@ -57,19 +93,12 @@ def replay():
     pass
 
 
-def display_word(word):
-    display = ""
-    for letter in word:
-        display = display + " " + letter
-    print(display)
-
-
-
 
 def game():
     """
     Method to contain the game loop
     """
+
     title()
     instructions()
     words = startup()
